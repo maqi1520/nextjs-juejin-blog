f57a8b666d88564149b8ec61df2a76e7586fe9f5
LeePeter <74251905+desiyc@users.noreply.github.com>
2023-12-26 14:32:50 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const config = {
-  title: " 狂奔滴小马 | 前端工程师成长部落阁",
+  title: " peter | 阅读",
   description: "分享 JavaScript 以及热门框架，记录前端工程师学习成长历程。",
-  author: "狂奔滴小马",
+  author: "peter",
   avatar:
     "https://p3-passport.byteimg.com/img/user-avatar/585e1491713363bc8f67d06c485e8260~200x200.awebp",
-  banner: "Xiaoma Blog",
+  banner: "Peter Blog",
 };
