10a936f09af4c79cfe40e3e9c7d0d521295ba229
LeePeter <74251905+desiyc@users.noreply.github.com>
2023-12-26 15:42:15 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config = {
   title: " Peter's Blog’",
   description: "分享 JavaScript 以及热门框架，记录前端工程师学习成长历程。",
-  author: "peter",
+  author: "Peter",
   avatar:
     "https://p3-passport.byteacctimg.com/img/user-avatar/3227254b000ad5e103c72b91365dd4a0~90x90.awebp",
   banner: "Peter Blog",
